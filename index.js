80b2c7690c50c6e4d208bc75bafb20aea497f784
Kashif1Naqvi <naqvishah582@gmail.com>
2019-12-09 07:23:47 +0500

    change data

---
+++ index.js
@@ -0,0 +1,101 @@
+function render(data){
+  let width = window.innerWidth,
+      height= window.innerHeight,
+      margin = {
+        top:150,
+        left:120,
+        right:80,
+        bottom:90
+      },
+      xValue = d=>d.population ,
+      tickNumber = number=>d3.format(".3s")(number).replace("G","B")
+      yValue = d=>d.country ,
+      barwidth = width - margin.left -margin.right,
+      barHeight = height - margin.top - margin.bottom;
+
+
+      const x = d3.scaleLinear()
+        .domain([0,d3.max(data,xValue)])
+        .range([0,barwidth]).nice()
+      const xAxis = d3.axisBottom(x).ticks(9).tickFormat(tickNumber).tickSize(-barHeight).tickPadding(15)
+      const y = d3.scalePoint()
+        .domain(data.map(yValue))
+        .range([0,barHeight])
+      const yAxis = d3.axisLeft(y).tickSize(-barwidth).tickPadding(11)
+      const svg = d3.select("#chart").append("svg")
+                    .attr("viewBox",`0 0 ${width} ${height}`)
+                    .style("background-color","#F5F2F3");
+      const g = svg.append("g").attr("transform",`translate(${margin.top},${margin.left})`)
+      let tooltip =d3.select("#chart").append("g").attr("transform",`translate(0,${height/2  })`).attr("class","tooltips")
+      let circle = g.selectAll("circle")
+                  .data(data)
+                  .enter()
+                  .append("circle")
+                  .attr("cx",x(10))
+                  .attr("cy",122)
+                  .attr("r",16)
+                  .on("mouseover",function(d,i){
+                    tooltip.html(`<div><p>${d.country} <br><b>Population:</b><i>${tickNumber(d.population)}</></p> </div>`)
+                    .style("top",  (d3.event.pageY ) + "px")
+                    .style("right",(d3.event.pageX )+ "px")
+
+                  })
+                  .on("mouseout",function(d,i){
+                    tooltip.html("")
+                  })
+      circle.transition()
+         .duration(2500)
+         .attr("cx",d=>x(xValue(d)))
+         .attr("cy",d=>y(yValue(d)))
+      let textTop = g.append("text").attr("x",innerWidth/2 - 100).attr("y",-12).text("Country Popolation scatter plot").attr("text-anchor","middle").attr("class","title")
+      let textBottom = g.append("text").attr("x",innerWidth/2 - 100).attr("y",barHeight+80).text("Population").attr("text-anchor","middle").attr("class","bottom")
+      let textLeft = g.append("text").attr("x",190).attr("y",131).text("Country").attr("text-anchor","middle").attr("class","left").attr("transform","rotate(90)")
+      let xGroup = g.append("g").call(xAxis).attr("transform",`translate(0,${barHeight})`)
+      let yGroup = g.append("g").call(yAxis).attr("class","yAxis")
+      xGroup.select(".domain").remove()
+      yGroup.selectAll('.domain').remove()
+      
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d3.csv("datas.csv").then(data=>{
+   data.forEach(d => {
+     d.horsepower = +d.horsepower
+     d.weight = +d.weight
+
+   });
+   render(data)
+  })
